e20dcb690f5b22ec22da0017e08f37ae578b6964
Bruna Girão <brunagirao.s@gmail.com>
2023-08-28 17:46:45 -0300

    p1: assignment

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -26,3 +26,18 @@ let myCurrentJob = 'Programmer';
 // don't use variable with
 let job1 = 'Teacher';
 let job2 = 'Programmer';
+
+/*****************Assignment*********************/
+/*
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console 
+*/
+
+let country = 'Brazil';
+let continent = 'South America';
+let population = '400000 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
