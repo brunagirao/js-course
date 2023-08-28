2afa0effd7cb9c377a3864caadd10c4ca077ce17
Bruna Girão <brunagirao.s@gmail.com>
2023-08-28 17:42:58 -0300

    p1: Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,3 +4,25 @@ let js = 'Amazing';
 if (js === 'Amazing') alert('JavaScript is FUN!');
 
 console.log(80+9+6/6);
+
+
+/*****************Values and Variables*********************/
+let firstName = 'Bruna';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'Jonas';
+let PI = '3.1415'; //constant
+
+// best approach
+let myFirstJob = 'Teacher';
+let myCurrentJob = 'Programmer';
+
+// don't use variable with
+let job1 = 'Teacher';
+let job2 = 'Programmer';
